79a3e138e75ef900ef47295ae8a103e832d91bb8
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-09-07 16:02:07 -0400

    moved files around

--- src/app/components/shared/footer/index.tsx
+++ src/app/components/shared/footer/index.tsx
@@ -1,53 +1,53 @@
 import styled from "styled-components";
 import {
-  Instagram,
-  LinkedIn,
-  Twitter,
-} from "../../../../../public/footer-icons";
+    Instagram,
+    LinkedIn,
+    Twitter,
+} from "./footer-icons";
 
 const Socials = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 40px;
-  position: absolute;
-  bottom: 10vh;
-  left: 11vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+    position: absolute;
+    bottom: 10vh;
+    left: 11vw;
 `;
 
 const Icon = styled.a`
-  &:hover {
-    cursor: pointer;
-    transition: 0.3s ease-out all;
-  }
+    &:hover {
+        cursor: pointer;
+        transition: 0.3s ease-out all;
+    }
 `;
 
 const SocialsContainer = () => {
-  return (
-    <Socials>
-      <Icon
-        href="https://www.instagram.com/uofthacks"
-        target="_blank"
-        rel="noopener"
-      >
-        <Instagram />
-      </Icon>
-      <Icon
-        href="https://www.twitter.com/uofthacks"
-        target="_blank"
-        rel="noopener"
-      >
-        <Twitter />
-      </Icon>
-      <Icon
-        href="https://www.linkedin.com/company/uoftorontohacks"
-        target="_blank"
-        rel="noopener"
-      >
-        <LinkedIn />
-      </Icon>
-    </Socials>
-  );
+    return (
+        <Socials>
+            <Icon
+                href="https://www.instagram.com/uofthacks"
+                target="_blank"
+                rel="noopener"
+            >
+                <Instagram/>
+            </Icon>
+            <Icon
+                href="https://www.twitter.com/uofthacks"
+                target="_blank"
+                rel="noopener"
+            >
+                <Twitter/>
+            </Icon>
+            <Icon
+                href="https://www.linkedin.com/company/uoftorontohacks"
+                target="_blank"
+                rel="noopener"
+            >
+                <LinkedIn/>
+            </Icon>
+        </Socials>
+    );
 };
 
 export default SocialsContainer;
