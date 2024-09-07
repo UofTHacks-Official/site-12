e7716dff1950a0fbf41ce44f9f27d8c19e38bdfa
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-09-07 17:14:08 -0400

    background improvements

--- src/app/modules/past-hackathons/index.styles.tsx
+++ src/app/modules/past-hackathons/index.styles.tsx
@@ -2,9 +2,8 @@ import styled from "styled-components";
 import {ModuleBackground, ModuleContainer} from "@/app/components/shared/containers/index.styles";
 
 const PastHackathonsModuleContainer = styled(ModuleContainer)`
-    height: 90vh;
+    height: 100vh;
     min-height: 600px;
-    max-height: 900px;
 `;
 
 const PastHackathonsModuleBackground = styled(ModuleBackground)``
