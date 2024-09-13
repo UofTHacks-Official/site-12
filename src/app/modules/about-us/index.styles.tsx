6015e7d9ea103602ca768ebf8aaa21c076935b37
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2024-09-13 16:03:35 -0400

    rough first layout

--- src/app/modules/about-us/index.styles.tsx
+++ src/app/modules/about-us/index.styles.tsx
@@ -1,8 +1,68 @@
 import styled from "styled-components";
-import {ModuleBackground, ModuleContainer} from "@/app/components/shared/containers/index.styles";
+import { ModuleBackground, ModuleContainer } from "@/app/components/shared/containers/index.styles";
 
-const AboutUsModuleContainer = styled(ModuleContainer)``;
+const AboutUsModuleContainer = styled(ModuleContainer)`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem;
+    position: relative;
+    margin: 0 auto;
+`;
 
-const AboutUsModuleBackground = styled(ModuleBackground)``;
+const AboutUsModuleBackground = styled(ModuleBackground)`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    background-size: cover;
+    background-position: center;
+`;
 
-export {AboutUsModuleContainer, AboutUsModuleBackground};
+const LeftContainer = styled.div`
+    max-width: 50vw;
+    padding-right: 3vw;
+    padding-left: 2vw;
+
+    h1 {
+        letter-spacing: 0px;
+        font-size: 60px;
+        font-weight: 700;
+        margin-bottom: 1.5rem;
+        color: #fff;
+        text-align: left;
+        line-height: 60px;
+        font-style: normal;
+    }
+`;
+
+const AboutUsDescription = styled.div`
+    background-color: #fff;
+    border-radius: 8px;
+    padding: 1rem 2rem;
+    margin-bottom: 2rem;
+    font-size: 16px;
+    text-align: left;
+    letter-spacing: 0px;
+`;
+
+const ImagesContainer = styled.div`
+    align-items: center;
+
+    img {
+        border: 10px solid #fff;
+        border-radius: 4px;
+        object-fit: cover;
+    }
+
+`;
+
+export {
+    AboutUsModuleContainer,
+    AboutUsModuleBackground,
+    LeftContainer,
+    AboutUsDescription,
+    ImagesContainer
+};
--- src/app/modules/about-us/index.tsx
+++ src/app/modules/about-us/index.tsx
@@ -1,14 +1,40 @@
-import SpaceGrotesk from "@/app/components/shared/fonts/space-grotesk";
-import {AboutUsModuleBackground, AboutUsModuleContainer} from "@/app/modules/about-us/index.styles";
+import { AboutUsModuleBackground, AboutUsModuleContainer, LeftContainer, AboutUsDescription, RightContainer, ImagesContainer } from "@/app/modules/about-us/index.styles";
 import Manrope from "@/app/components/shared/fonts/manrope";
+import Image from "next/image";
+import SpaceGrotesk from "@/app/components/shared/fonts/space-grotesk";
 
 const AboutUs = () => {
-    return (
-        <AboutUsModuleContainer>
-            <AboutUsModuleBackground src="/background/about-us.svg"/>
-            <Manrope>about us section</Manrope>
-        </AboutUsModuleContainer>
-    );
+  const aboutUsDescription = (
+    <>
+      <p>
+        At UofTHacks, we strive to provide a space where everyone feels supported, empowered, and encouraged to transform their dreams into reality!
+      </p>
+      <br></br>
+      <p>
+        Our event brings together over 500 innovators, developers, designers, and entrepreneurs for a memorable 36-hour in-person hackathon, where they collaboratively craft unique and meaningful projects.
+      </p>
+    </>
+  );
+
+  return (
+    <AboutUsModuleContainer>
+        <AboutUsModuleBackground src="/background/about-us.svg" />
+        <LeftContainer>
+            <SpaceGrotesk>
+                <h1>Canada's first <br></br> student-run hackathon</h1>
+            </SpaceGrotesk>
+            <Manrope>
+                <AboutUsDescription>{aboutUsDescription}</AboutUsDescription>
+            </Manrope>
+        </LeftContainer>
+
+        <ImagesContainer>
+            <Image src="/assets/about/about-1.svg" alt="UofT Hacks executive speaking to a crowd" width={160} height={160} />
+            <Image src="/assets/about/about-2.svg" alt="UofT Hacks sponsors" width={350} height={200} />
+            <Image src="/assets/about/about-3.svg" alt="UofT Hacks executive speaking to a crowd" width={400} height={200} />
+        </ImagesContainer>
+    </AboutUsModuleContainer>
+  );
 };
 
 export default AboutUs;
