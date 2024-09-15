a1251dd90a36a9373e7abcea283ccc78549d66b5
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-09-14 20:23:53 -0400

    contact us clean up

--- src/app/modules/contact-us/index.styles.tsx
+++ src/app/modules/contact-us/index.styles.tsx
@@ -1,99 +1,103 @@
 import styled from "styled-components";
-import {
-  ModuleBackground,
-  ModuleContainer,
-} from "@/app/components/shared/containers/index.styles";
+import {ModuleBackground, ModuleContainer} from "@/app/components/shared/containers/index.styles";
+import SpaceGrotesk from "@/app/components/shared/fonts/space-grotesk";
+import Manrope from "@/app/components/shared/fonts/manrope";
+
+type MobileProps = {
+    isMobile?: boolean | null;
+}
 
 export const ContactUsModuleContainer = styled(ModuleContainer)`
-  height: 100vh;
-  min-height: 600px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+    height: 100vh;
+    min-height: 600px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const ContactUsModuleBackground = styled(ModuleBackground)``;
 
-export const InputWrapper = styled.div`
-  display: flex;
-  gap: 20px;
+export const InputWrapper = styled.div<MobileProps>`
+    display: flex;
+    flex-direction: ${(props) => (props.isMobile ? "column" : "row")};
+    gap: 20px;
 `;
 
-export const FormContainer = styled.form`
-  margin-top: 40px;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  max-width: 500px;
-  width: 100%;
+export const FormContainer = styled.form<MobileProps>`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    max-width: ${(props) => (props.isMobile ? "100%" : "500px")};
+    width: 100%;
 `;
 
-export const Container = styled.div`
-  width: 100%; /* Adjust the width as needed */
-  text-align: left;
-  margin-left: -300px;
+export const Container = styled.div<MobileProps>`
+    text-align: left;
+    padding: ${({isMobile}) => (isMobile ? "20px" : "0")};
 `;
 
-export const StyledHeader = styled.h3`
-  color: var(--Neutral-600, #191a1b);
-  font-family: var(--Heading2-fontFamily, "Space Grotesk");
-  font-size: var(--Heading2-size, 45px);
-  font-style: normal;
-  font-weight: var(--Heading2-weight, 700);
-  line-height: var(--Heading2-lineHeight, 72px); /* 120% */
-  letter-spacing: var(--Heading3-spacing, 0px);
+export const StyledHeader = styled(SpaceGrotesk)<MobileProps>`
+    color: var(--Neutral-600, #191a1b);
+    font-size: ${({isMobile}) => (isMobile ? "32px" : "45px")};
+    font-weight: var(--Heading2-weight, 700);
+    line-height: ${({isMobile}) => (isMobile ? "48px" : "72px")};
+    margin-bottom: ${({isMobile}) => (isMobile ? "10px" : "20px")};
+    text-align: left;
+    letter-spacing: 0rem;
 `;
 
-export const StyledParagraph = styled.p`
-  width: 549px;
-  height: 63px;
-  flex-shrink: 0;
-  color: var(--Neutral-600, #191a1b);
-  font-family: var(--Subtitle1-fontFamily, Manrope);
-  font-size: var(--Subtitle1-lineHeight, 24px);
-  font-style: normal;
-  font-weight: var(--Subtitle1-weight, 600);
-  line-height: var(--Subtitle1-lineHeight, 36px); /* 100% */
-  letter-spacing: var(--Subtitle1-spacing, 0px);
+export const StyledParagraph = styled(Manrope)<MobileProps>`
+    width: ${({isMobile}) => (isMobile ? "100%" : "549px")};
+    color: var(--Neutral-600, #191a1b);
+    font-size: ${({isMobile}) => (isMobile ? "18px" : "24px")};
+    font-weight: var(--Subtitle1-weight, 600);
+    line-height: ${({isMobile}) => (isMobile ? "28px" : "36px")};
+    text-align: left;
+    margin-bottom: 20px;
 `;
 
-export const Input = styled.input`
-  border-radius: 10px;
-  border: 1px solid #8edeff;
-  background: #fff;
-  box-shadow: 0px 0px 50px 0px rgba(182, 255, 246, 0.5);
-  display: flex;
-  width: 260px;
-  height: 55px;
-  padding: 10px 20px;
-  align-items: flex-start;
-  flex-shrink: 0;
+export const Input = styled.input<MobileProps>`
+    border-radius: 10px;
+    border: 1px solid #8edeff;
+    background: #fff;
+    box-shadow: 0px 0px 50px 0px rgba(182, 255, 246, 0.5);
+    width: ${({isMobile}) => (isMobile ? "100%" : "260px")};
+    height: 55px;
+    font-family: 'Manrope', sans-serif;
+    padding: 10px 20px;
 `;
 
-export const TextArea = styled.textarea`
-  display: flex;
-  width: 550px;
-  height: 110px;
-  padding: 8px 18px;
-  align-items: flex-start;
-  flex-shrink: 0;
-  border-radius: 10px;
-  border: 1px solid #8edeff;
-  background: #fff;
-  box-shadow: 0px 0px 50px 0px rgba(182, 255, 246, 0.5);
+export const TextArea = styled.textarea<MobileProps>`
+    width: 100%;
+    height: 110px;
+    padding: 18px;
+    border-radius: 10px;
+    border: 1px solid #8edeff;
+    background: #fff;
+    font-family: 'Manrope', sans-serif;
+    box-shadow: 0px 0px 50px 0px rgba(182, 255, 246, 0.5);
 `;
 
-export const SubmitButton = styled.button`
-  display: inline-flex;
-  padding: 16px 60px;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  flex-shrink: 0;
-  border-radius: 16px;
-  background: #225c90;
-  backdrop-filter: blur(6px);
-  width: 150px;
-  height: 45px;
+export const ManropeSubmitButtonText = styled(Manrope)`
+    color: var(--Neutral-100, #FFF);
+    font-size: var(--Subtitle2-size, 15px);
+    font-weight: var(--Subtitle2-weight, 600);
+    line-height: var(--Subtitle2-lineHeight, 32px);
+`;
+
+export const SubmitButton = styled.button<MobileProps>`
+    border-radius: 16px;
+    border: 0px;
+    color: white;
+    background: #225c90;
+    width: ${({isMobile}) => (isMobile ? "100%" : "150px")};
+    height: 45px;
+    transition: background 0.3s ease;
+
+    &:hover {
+        background: #1a4871;
+        box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.2);
+        cursor: pointer;
+    }
 `;
--- src/app/modules/contact-us/index.tsx
+++ src/app/modules/contact-us/index.tsx
@@ -1,76 +1,84 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {
-  ContactUsModuleContainer,
-  ContactUsModuleBackground,
-  FormContainer,
-  Input,
-  TextArea,
-  SubmitButton,
-  StyledHeader,
-  StyledParagraph,
-  Container,
-  InputWrapper,
+    ContactUsModuleContainer,
+    FormContainer,
+    Input,
+    TextArea,
+    SubmitButton,
+    StyledHeader,
+    StyledParagraph,
+    Container,
+    InputWrapper,
+    ManropeSubmitButtonText, ContactUsModuleBackground,
 } from "./index.styles";
-import SpaceGrotesk from "@/app/components/shared/fonts/space-grotesk";
+import {useMobileDetect} from "@/app/hooks/useMobileDetect";
 
 const ContactUs = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    reason: "",
-  });
+    const isMobile = useMobileDetect();
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        reason: "",
+    });
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
+    const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        const {name, value} = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form data:", formData);
-    // Here you would typically send the data to your backend
-  };
-  return (
-    <ContactUsModuleContainer id="Contact-module">
-      <ContactUsModuleBackground src="/background/contact-us.svg" />
-      <SpaceGrotesk>
-        <Container>
-          <StyledHeader>Contact Us</StyledHeader>
-          <StyledParagraph>Interested in learning more?</StyledParagraph>
-          <FormContainer onSubmit={handleSubmit}>
-            <InputWrapper>
-              <Input
-                type="text"
-                name="name"
-                placeholder="Name"
-                value={formData.name}
-                onChange={handleChange}
-              />
-              <Input
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleChange}
-              />
-            </InputWrapper>
-            <TextArea
-              name="reason"
-              placeholder="Reason for contacting us..."
-              value={formData.reason}
-              onChange={handleChange}
-            />
-            <SubmitButton type="submit">Submit</SubmitButton>
-          </FormContainer>
-        </Container>
-      </SpaceGrotesk>
-    </ContactUsModuleContainer>
-  );
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Form data:", formData);
+    };
+
+    return (
+        <ContactUsModuleContainer id="Contact-module">
+            <ContactUsModuleBackground src="/background/contact-us.svg"/>
+            <Container isMobile={isMobile}>
+                <StyledHeader isMobile={isMobile}>Contact Us</StyledHeader>
+                <StyledParagraph isMobile={isMobile}>
+                    Interested in learning more?
+                </StyledParagraph>
+                <FormContainer onSubmit={handleSubmit} isMobile={isMobile}>
+                    <InputWrapper isMobile={isMobile}>
+                        <Input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            isMobile={isMobile}
+                        />
+                        <Input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            isMobile={isMobile}
+                        />
+                    </InputWrapper>
+                    <TextArea
+                        name="reason"
+                        placeholder="Reason for contacting us..."
+                        value={formData.reason}
+                        onChange={handleChange}
+                        isMobile={isMobile}
+                    />
+                    <SubmitButton type="submit" isMobile={isMobile}>
+                        <ManropeSubmitButtonText>
+                            Submit
+                        </ManropeSubmitButtonText>
+                    </SubmitButton>
+                </FormContainer>
+            </Container>
+        </ContactUsModuleContainer>
+    );
 };
 
 export default ContactUs;
