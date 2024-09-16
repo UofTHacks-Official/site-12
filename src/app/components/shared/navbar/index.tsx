fe7a34a9423fd4c5395379b19e303a6e4f3e1758
YannickLongval <yannicklongval2014@gmail.com>
2024-09-16 19:49:06 -0400

    navbar iterations

--- src/app/components/shared/navbar/index.tsx
+++ src/app/components/shared/navbar/index.tsx
@@ -1,178 +1,185 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import {useMobileDetect} from "@/app/hooks/useMobileDetect";
+import { useMobileDetect } from "@/app/hooks/useMobileDetect";
 import Image from "next/image";
 import {
-    Logo,
-    MobileMenu,
-    NavLink,
-    NavLinkItem,
-    NavLinks,
-    NavigationBar,
-    NavigationContainer, HamburgerMenu, MobileNavLinkItem,
-} from "./index.styles"
+  Logo,
+  MobileMenu,
+  NavLink,
+  NavLinkItem,
+  NavLinks,
+  NavigationBar,
+  NavigationContainer,
+  HamburgerMenu,
+  MobileNavLinkItem,
+} from "./index.styles";
 import Manrope from "../fonts/manrope";
 
-const NavBarLogo = "/assets/navbar_logo.svg"
+const NavBarLogo = "/assets/navbar_logo.svg";
 
 const NavBar = () => {
-    const [isNavBarVisible, setNavBarVisible] = useState(true);
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isNavBarVisible, setNavBarVisible] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const isMobile = useMobileDetect();
-    const [mobileMenuNode, setMobileMenuNode] = useState<HTMLSpanElement | null>(
-        null
-    );
+  const isMobile = useMobileDetect();
+  const [mobileMenuNode, setMobileMenuNode] = useState<HTMLSpanElement | null>(
+    null
+  );
 
-    const [hamburgerNode, setHamburgerNode] = useState<HTMLDivElement | null>(
-        null
-    );
+  const [hamburgerNode, setHamburgerNode] = useState<HTMLDivElement | null>(
+    null
+  );
 
-    const scrollToSection = (sectionId: string) => {
-        const section = document.getElementById(sectionId);
-        if (section) {
-            section.scrollIntoView({behavior: "smooth"});
-        }
-    };
-
-    const hackerPortalLink = "https://portal.uofthacks.com"
-    const onHackerPortalClick = () => {
-        window.open(hackerPortalLink);
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+      setIsMenuOpen(false);
     }
+  };
 
-    const [prevScrollY, setPrevScrollY] = useState(0); // Track previous scroll position
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollY = window.scrollY;
-            const scrollDirection = scrollY > prevScrollY ? "down" : "up";
-            if (scrollY < 70) {
-                setNavBarVisible(true);
-                return;
-            }
+  const hackerPortalLink = "https://portal.uofthacks.com";
+  const onHackerPortalClick = () => {
+    window.open(hackerPortalLink);
+  };
 
-            // Determine visibility based on scroll direction
-            if (scrollDirection === "down") {
-                setNavBarVisible(false);
-                setIsMenuOpen(false);
-            } else {
-                setNavBarVisible(true);
-            }
+  const [prevScrollY, setPrevScrollY] = useState(0); // Track previous scroll position
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const scrollDirection = scrollY > prevScrollY ? "down" : "up";
+      if (scrollY < 70) {
+        setNavBarVisible(true);
+        return;
+      }
 
-            setPrevScrollY(scrollY); // Update previous scroll position
-        };
+      // Determine visibility based on scroll direction
+      if (scrollDirection === "down") {
+        setNavBarVisible(false);
+        setIsMenuOpen(false);
+      } else {
+        setNavBarVisible(true);
+      }
 
-        window.addEventListener("scroll", handleScroll);
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, [prevScrollY]);
-
-    useEffect(() => {
-        const handleClickOutside = (event: any) => {
-            if (
-                isMenuOpen &&
-                mobileMenuNode &&
-                !mobileMenuNode.contains(event.target) &&
-                !hamburgerNode?.contains(event.target)
-            ) {
-                setIsMenuOpen(false);
-            }
-        };
-
-        document.addEventListener("click", handleClickOutside);
-
-        return () => {
-            document.removeEventListener("click", handleClickOutside);
-        };
-    }, [isMenuOpen]);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollY = window.scrollY;
-            if (scrollY > 0) {
-                setNavBarVisible(false);
-            } else {
-                setNavBarVisible(true);
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
+      setPrevScrollY(scrollY); // Update previous scroll position
     };
 
-    const navManropeStyle = () => ({
-        fontSize: "17px",
-        display: "inline",
-        letterSpacing: "0px",
-        color: "white",
-    });
+    window.addEventListener("scroll", handleScroll);
 
-    return (
-        <>
-            <NavigationContainer open={isNavBarVisible}>
-                <NavigationBar>
-                    <Logo onClick={() => scrollToSection("start")}>
-                        <Image src={NavBarLogo} alt="UofTHacks Logo" width={80} height={80}/>
-                    </Logo>
-                    <NavLinks>
-                        <div style={{display: isMobile ? "none" : "flex", gap: 10}}>
-                            <NavLinkItem>
-                                <NavLink onClick={() => scrollToSection("Sponsors-module")}>
-                                    <Manrope style={navManropeStyle()}>sponsors</Manrope>
-                                </NavLink>
-                            </NavLinkItem>
-                            <NavLinkItem>
-                                <NavLink onClick={() => scrollToSection("FAQ-module")}>
-                                    <Manrope style={navManropeStyle()}>faq</Manrope>
-                                </NavLink>
-                            </NavLinkItem>
-                            <NavLinkItem>
-                                <NavLink onClick={() => scrollToSection("Contact-module")}>
-                                    <Manrope style={navManropeStyle()}>contact</Manrope>
-                                </NavLink>
-                            </NavLinkItem>
-                        </div>
-                        <HamburgerMenu
-                            ref={(node) => setHamburgerNode(node)}
-                            mobile={isMobile}
-                            onClick={toggleMenu}
-                        >
-                            <Manrope>☰</Manrope>
-                        </HamburgerMenu>
-                    </NavLinks>
-                </NavigationBar>
-            </NavigationContainer>
-            {isMobile && (
-                <span ref={(node) => setMobileMenuNode(node)}>
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollY]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (
+        isMenuOpen &&
+        mobileMenuNode &&
+        !mobileMenuNode.contains(event.target) &&
+        !hamburgerNode?.contains(event.target)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isMenuOpen]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      if (scrollY > 0) {
+        setNavBarVisible(false);
+      } else {
+        setNavBarVisible(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navManropeStyle = () => ({
+    fontSize: "17px",
+    display: "inline",
+    letterSpacing: "0px",
+    color: "white",
+  });
+
+  return (
+    <>
+      <NavigationContainer open={isNavBarVisible}>
+        <NavigationBar>
+          <Logo onClick={() => scrollToSection("start")}>
+            <Image
+              src={NavBarLogo}
+              alt="UofTHacks Logo"
+              width={isMobile ? 75 : 120}
+              height={isMobile ? 50 : 80}
+            />
+          </Logo>
+          <NavLinks>
+            <div style={{ display: isMobile ? "none" : "flex", gap: 10 }}>
+              <NavLinkItem>
+                <NavLink onClick={() => scrollToSection("Sponsors-module")}>
+                  <Manrope style={navManropeStyle()}>Sponsors</Manrope>
+                </NavLink>
+              </NavLinkItem>
+              <NavLinkItem>
+                <NavLink onClick={() => scrollToSection("FAQ-module")}>
+                  <Manrope style={navManropeStyle()}>FAQ</Manrope>
+                </NavLink>
+              </NavLinkItem>
+              <NavLinkItem>
+                <NavLink onClick={() => scrollToSection("Contact-module")}>
+                  <Manrope style={navManropeStyle()}>Contact Us</Manrope>
+                </NavLink>
+              </NavLinkItem>
+            </div>
+            <HamburgerMenu
+              ref={(node) => setHamburgerNode(node)}
+              mobile={isMobile}
+              onClick={toggleMenu}
+            >
+              <Manrope style={navManropeStyle()}>☰</Manrope>
+            </HamburgerMenu>
+          </NavLinks>
+        </NavigationBar>
+      </NavigationContainer>
+      {isMobile && (
+        <span ref={(node) => setMobileMenuNode(node)}>
           <MobileMenu open={isMenuOpen}>
-              <MobileNavLinkItem>
+            <MobileNavLinkItem>
               <NavLink onClick={() => scrollToSection("Sponsors-module")}>
-                <Manrope style={navManropeStyle()}>sponsors</Manrope>
+                <Manrope style={navManropeStyle()}>Sponsors</Manrope>
               </NavLink>
-
-                  </MobileNavLinkItem>
+            </MobileNavLinkItem>
             <MobileNavLinkItem>
               <NavLink onClick={() => scrollToSection("FAQ-module")}>
-                <Manrope style={navManropeStyle()}>faq</Manrope>
+                <Manrope style={navManropeStyle()}>FAQ</Manrope>
               </NavLink>
             </MobileNavLinkItem>
             <MobileNavLinkItem>
               <NavLink onClick={() => scrollToSection("Contact-module")}>
-                <Manrope style={navManropeStyle()}>contact</Manrope>
+                <Manrope style={navManropeStyle()}>Contact Us</Manrope>
               </NavLink>
             </MobileNavLinkItem>
           </MobileMenu>
         </span>
-            )}
-        </>
-    );
+      )}
+    </>
+  );
 };
 
 export default NavBar;
