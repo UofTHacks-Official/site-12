7e125a6417b6c648f28bbaf99a9b2dc8b1f0d452
IlKoKaIl <mihirkoka@gmail.com>
2024-09-23 20:06:39 -0400

    Initial changes to stats section

--- src/app/modules/stats/index.styles.tsx
+++ src/app/modules/stats/index.styles.tsx
@@ -1,9 +1,42 @@
 import styled from "styled-components";
-import {ModuleBackground, ModuleContainer} from "@/app/components/shared/containers/index.styles";
+import {
+  ModuleBackground,
+  ModuleContainer,
+} from "@/app/components/shared/containers/index.styles";
 
 const StatsModuleContainer = styled(ModuleContainer)`
+  position: relative;
+  overflow: hidden;
+  height: 140vh;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    height: 100vh; // Reduce height on mobile
+    align-items: flex-start; // Align content to top on mobile
+    padding-top: 2rem; // Add some top padding on mobile
+  }
 `;
 
-const StatsModuleBackground = styled(ModuleBackground)``
+const StatsModuleBackground = styled(ModuleBackground)`
+  height: 140vh;
 
-export {StatsModuleContainer, StatsModuleBackground};
+  @media (max-width: 768px) {
+    height: 100vh; // Match container height on mobile
+  }
+`;
+
+const StatsImageWrapper = styled.div`
+  position: relative;
+  width: 90vw;
+  height: 100%;
+  margin-left: 0;
+
+  @media (max-width: 768px) {
+    width: 150vw; // mobile width
+    height: 80%; // Reduce height to prevent overflow
+    overflow: hidden;
+  }
+`;
+
+export { StatsModuleContainer, StatsModuleBackground, StatsImageWrapper };
--- src/app/modules/stats/index.tsx
+++ src/app/modules/stats/index.tsx
@@ -1,13 +1,27 @@
-import {StatsModuleContainer, StatsModuleBackground} from "@/app/modules/stats/index.styles";
+import {
+  StatsModuleContainer,
+  StatsModuleBackground,
+  StatsImageWrapper,
+} from "@/app/modules/stats/index.styles";
 import SpaceGrotesk from "@/app/components/shared/fonts/space-grotesk";
+import BooksSVG from "./Main Books.svg";
+import Image from "next/image";
 
 const Stats = () => {
-    return (
-        <StatsModuleContainer id="Stats-module">
-            <StatsModuleBackground src="/background/stats.svg"/>
-            <SpaceGrotesk>Stats section</SpaceGrotesk>
-        </StatsModuleContainer>
-    );
+  return (
+    <StatsModuleContainer id="Stats-module">
+      <StatsModuleBackground src="/background/stats.svg" />
+      <StatsImageWrapper>
+        <Image
+          src={BooksSVG}
+          alt="My Design"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left center"
+        />
+      </StatsImageWrapper>
+    </StatsModuleContainer>
+  );
 };
 
 export default Stats;
