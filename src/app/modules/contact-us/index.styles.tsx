2f7304c987fd0f6c38f63598955419f8201a63dc
IlKoKaIl <mihirkoka@gmail.com>
2024-09-30 22:15:27 -0400

    added room svg to contact us section

--- src/app/modules/contact-us/index.styles.tsx
+++ src/app/modules/contact-us/index.styles.tsx
@@ -1,24 +1,20 @@
 import styled from "styled-components";
-import {ModuleBackground, ModuleContainer} from "@/app/components/shared/containers/index.styles";
-import SpaceGrotesk from "@/app/components/shared/fonts/space-grotesk";
-import Manrope from "@/app/components/shared/fonts/manrope";
-
-type MobileProps = {
-    isMobile?: boolean | null;
-}
+import {
+  ModuleBackground,
+  ModuleContainer,
+} from "@/app/components/shared/containers/index.styles";
 
 export const ContactUsModuleContainer = styled(ModuleContainer)`
-    height: 100vh;
-    min-height: 600px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  height: 100vh;
+  min-height: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const ContactUsModuleBackground = styled(ModuleBackground)``;
 
-<<<<<<< HEAD
 export const ContentWrapper = styled.div`
   display: flex;
   justify-content: space-between;
@@ -26,7 +22,7 @@ export const ContentWrapper = styled.div`
   width: 100%;
   max-width: 1100px;
   margin: 0 auto;
-  gap: 50px;
+  gap: 40px;
 `;
 
 export const InputWrapper = styled.div`
@@ -122,88 +118,4 @@ export const IsometricRoomImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: contain;
-=======
-export const InputWrapper = styled.div<MobileProps>`
-    display: flex;
-    flex-direction: ${(props) => (props.isMobile ? "column" : "row")};
-    gap: 20px;
-`;
-
-export const FormContainer = styled.form<MobileProps>`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    max-width: ${(props) => (props.isMobile ? "100%" : "500px")};
-    width: 100%;
-`;
-
-export const Container = styled.div<MobileProps>`
-    text-align: left;
-    padding: ${({isMobile}) => (isMobile ? "20px" : "0")};
-`;
-
-export const StyledHeader = styled(SpaceGrotesk)<MobileProps>`
-    color: var(--Neutral-600, #191a1b);
-    font-size: ${({isMobile}) => (isMobile ? "32px" : "45px")};
-    font-weight: var(--Heading2-weight, 700);
-    line-height: ${({isMobile}) => (isMobile ? "48px" : "72px")};
-    margin-bottom: ${({isMobile}) => (isMobile ? "10px" : "20px")};
-    text-align: left;
-    letter-spacing: 0rem;
-`;
-
-export const StyledParagraph = styled(Manrope)<MobileProps>`
-    width: ${({isMobile}) => (isMobile ? "100%" : "549px")};
-    color: var(--Neutral-600, #191a1b);
-    font-size: ${({isMobile}) => (isMobile ? "18px" : "24px")};
-    font-weight: var(--Subtitle1-weight, 600);
-    line-height: ${({isMobile}) => (isMobile ? "28px" : "36px")};
-    text-align: left;
-    margin-bottom: 20px;
-`;
-
-export const Input = styled.input<MobileProps>`
-    border-radius: 10px;
-    border: 1px solid #8edeff;
-    background: #fff;
-    box-shadow: 0px 0px 50px 0px rgba(182, 255, 246, 0.5);
-    width: ${({isMobile}) => (isMobile ? "100%" : "260px")};
-    height: 55px;
-    font-family: 'Manrope', sans-serif;
-    padding: 10px 20px;
-`;
-
-export const TextArea = styled.textarea<MobileProps>`
-    width: 100%;
-    height: 110px;
-    padding: 18px;
-    border-radius: 10px;
-    border: 1px solid #8edeff;
-    background: #fff;
-    font-family: 'Manrope', sans-serif;
-    box-shadow: 0px 0px 50px 0px rgba(182, 255, 246, 0.5);
-`;
-
-export const ManropeSubmitButtonText = styled(Manrope)`
-    color: var(--Neutral-100, #FFF);
-    font-size: var(--Subtitle2-size, 15px);
-    font-weight: var(--Subtitle2-weight, 600);
-    line-height: var(--Subtitle2-lineHeight, 32px);
-`;
-
-export const SubmitButton = styled.button<MobileProps>`
-    border-radius: 16px;
-    border: 0px;
-    color: white;
-    background: #225c90;
-    width: ${({isMobile}) => (isMobile ? "100%" : "150px")};
-    height: 45px;
-    transition: background 0.3s ease;
-
-    &:hover {
-        background: #1a4871;
-        box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.2);
-        cursor: pointer;
-    }
->>>>>>> 38c49616a90457b38153e844869b9f105c831235
 `;
--- src/app/modules/contact-us/index.tsx
+++ src/app/modules/contact-us/index.tsx
@@ -1,6 +1,5 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
-<<<<<<< HEAD
   ContactUsModuleContainer,
   ContactUsModuleBackground,
   FormContainer,
@@ -14,40 +13,26 @@ import {
   IsometricRoomWrapper,
   IsometricRoomImage,
   ContentWrapper,
-=======
-    ContactUsModuleContainer,
-    FormContainer,
-    Input,
-    TextArea,
-    SubmitButton,
-    StyledHeader,
-    StyledParagraph,
-    Container,
-    InputWrapper,
-    ManropeSubmitButtonText, ContactUsModuleBackground,
->>>>>>> 38c49616a90457b38153e844869b9f105c831235
 } from "./index.styles";
-import {useMobileDetect} from "@/app/hooks/useMobileDetect";
+import SpaceGrotesk from "@/app/components/shared/fonts/space-grotesk";
 
 const ContactUs = () => {
-    const isMobile = useMobileDetect();
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        reason: "",
-    });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    reason: "",
+  });
 
-    const handleChange = (
-        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
-        const {name, value} = e.target;
-        setFormData((prevState) => ({
-            ...prevState,
-            [name]: value,
-        }));
-    };
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-<<<<<<< HEAD
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form data:", formData);
@@ -95,56 +80,6 @@ const ContactUs = () => {
       </SpaceGrotesk>
     </ContactUsModuleContainer>
   );
-=======
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log("Form data:", formData);
-    };
-
-    return (
-        <ContactUsModuleContainer id="Contact-module">
-            <ContactUsModuleBackground src="/background/contact-us.svg"/>
-            <Container isMobile={isMobile}>
-                <StyledHeader isMobile={isMobile}>Contact Us</StyledHeader>
-                <StyledParagraph isMobile={isMobile}>
-                    Interested in learning more?
-                </StyledParagraph>
-                <FormContainer onSubmit={handleSubmit} isMobile={isMobile}>
-                    <InputWrapper isMobile={isMobile}>
-                        <Input
-                            type="text"
-                            name="name"
-                            placeholder="Name"
-                            value={formData.name}
-                            onChange={handleChange}
-                            isMobile={isMobile}
-                        />
-                        <Input
-                            type="email"
-                            name="email"
-                            placeholder="Email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            isMobile={isMobile}
-                        />
-                    </InputWrapper>
-                    <TextArea
-                        name="reason"
-                        placeholder="Reason for contacting us..."
-                        value={formData.reason}
-                        onChange={handleChange}
-                        isMobile={isMobile}
-                    />
-                    <SubmitButton type="submit" isMobile={isMobile}>
-                        <ManropeSubmitButtonText>
-                            Submit
-                        </ManropeSubmitButtonText>
-                    </SubmitButton>
-                </FormContainer>
-            </Container>
-        </ContactUsModuleContainer>
-    );
->>>>>>> 38c49616a90457b38153e844869b9f105c831235
 };
 
 export default ContactUs;
