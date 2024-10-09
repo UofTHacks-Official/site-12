75934a55e0b1e5b43a39dbb8b8e2d1f0b62ff2e4
IlKoKaIl <mihirkoka@gmail.com>
2024-10-09 16:04:19 -0400

    change book svg used dynamically depending on screen size

--- src/app/modules/stats/index.styles.tsx
+++ src/app/modules/stats/index.styles.tsx
@@ -1,31 +1,36 @@
 import styled from "styled-components";
 import {
-    ModuleBackground,
-    ModuleContainer,
+  ModuleBackground,
+  ModuleContainer,
 } from "@/app/components/shared/containers/index.styles";
 
 interface MobileProps {
-    isMobile?: boolean | null;
+  isMobile?: boolean | null;
+  isLargeScreen?: boolean; // Add this prop for large screen detection
 }
 
 const StatsModuleContainer = styled(ModuleContainer)<MobileProps>`
-    position: relative;
-    overflow: hidden;
-    display: flex;
-    align-items: ${(props) => (props.isMobile ? 'flex-start' : 'center')};
-    padding-top: ${(props) => (props.isMobile ? '2rem' : '0')};
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  align-items: ${(props) => (props.isMobile ? "flex-start" : "center")};
+  padding-top: ${(props) => (props.isMobile ? "2rem" : "0")};
 `;
 
 const StatsModuleBackground = styled(ModuleBackground)`
-    height: 100vh;
+  height: 100vh;
 `;
 
 const StatsImageWrapper = styled.div<MobileProps>`
-    position: relative;
-    width: ${(props) => (props.isMobile ? '150vw' : '90vw')};
-    height: ${(props) => (props.isMobile ? '80%' : '100%')};
-    margin-left: 0;
-    overflow: ${(props) => (props.isMobile ? 'hidden' : 'visible')};
+  position: relative;
+  width: ${(props) =>
+    props.isMobile ? "150vw" : props.isLargeScreen ? "100vw" : "90vw"};
+  height: ${(props) => (props.isMobile ? "80%" : "100%")};
+  margin-left: ${(props) =>
+    props.isLargeScreen ? "auto" : "0"}; // Center on large screens
+  margin-right: ${(props) =>
+    props.isLargeScreen ? "auto" : "0"}; // Center on large screens
+  overflow: ${(props) => (props.isMobile ? "hidden" : "visible")};
 `;
 
-export {StatsModuleContainer, StatsModuleBackground, StatsImageWrapper};
+export { StatsModuleContainer, StatsModuleBackground, StatsImageWrapper };
