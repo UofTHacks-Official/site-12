0b290f38ee98cb446a066770a370bbcf05f99704
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-10-29 01:47:00 -0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,20 +5,25 @@ import "./globals.css";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "UofTHacks 12",
-  description: "Join 500+ hackers for a great weekend of learning, building, and networking at University of Toronto's largest annual hackathon 💻",
+interface RootLayoutProps {
+    children: ReactNode;
+}
+
+const metadata = {
+    title: "UofTHacks 12",
+    description:
+        "Join 500+ hackers for a great weekend of learning, building, and networking at University of Toronto's largest annual hackathon 💻",
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </Head>
-      <body className={manrope.className}>{children}</body>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>{metadata.title}</title>
+                <meta name="description" content={metadata.description} />
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+            </Head>
+            <body className={manrope.className}>{children}</body>
+        </>
+    );
 }
