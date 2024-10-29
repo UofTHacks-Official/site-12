e66d9a337aab1e4844fb2a63381e75c1dfaa71fc
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-10-28 22:22:24 -0400

    fix

--- src/app/modules/why-join/index.styles.tsx
+++ src/app/modules/why-join/index.styles.tsx
@@ -67,6 +67,7 @@ export const NoteWrapper = styled(Box)`
   border-radius: 8px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
   width: 100%;
+  color: #000000;
   display: flex;
   flex-direction: column;
   justify-content: center;
